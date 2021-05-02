b6de67ee4ccaf8dedc75838143133c4a57fc7be4
lichao <leetomlee123@gamil.com>
2021-05-02 19:33:42 +0800

    1111

--- checkin.py
+++ checkin.py
@@ -86,7 +86,7 @@ def get_books_from_db():
     #wait(jobs, return_when=ALL_COMPLETED)
     for i in range(len(ids)):
         updateBook(ids[i],links[i])
-        # time.sleep(5)
+        # time.sleep(2)
         logging.info("update "+str(books[i]))
         
     logging.info("update %s books" % str(cnt))
